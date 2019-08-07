01d40a3676a086a41001008528f426aead9d70db
jee yun lee <jy617.lee@gmail.com>
2019-08-07 21:33:29 +0900

    landing page

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, {Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends Component{
+  state = {
+    isLoaded: false
+  }
+
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        { isLoaded ? null: (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting Weather Now</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  loading:{
+    flex:1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 93,
+
+  }
+});
